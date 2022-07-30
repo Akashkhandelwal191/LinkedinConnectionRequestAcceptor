cbb1c922281710668d74bea3443cc5a3d6944634
Akash khandelwal <skykhandelwal1999@gmail.com>
2022-07-30 16:57:25 +0530

    All Files Added

---
+++ content.js
@@ -0,0 +1,15 @@
+
+setInterval(() => {
+    
+   var connection = document.querySelectorAll(".invitation-card__action-btn.artdeco-button--secondary");
+        
+        for(var i = 0; i < connection.length; i = i + 1)
+        {
+           connection[i].click();
+
+        }
+
+}, 2000);
+
+
+
